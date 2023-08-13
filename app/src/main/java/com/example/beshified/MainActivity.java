d2e212650058dad2efdbfb999299f40c448748ea
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2023-08-13 11:35:55 +0800

    added a button to copy text using feature using ClipboarManager

--- app/src/main/java/com/example/beshified/MainActivity.java
+++ app/src/main/java/com/example/beshified/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.beshified;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText textBeshify;
     private Button btnBeshify;
     private TextView beshifiedText;
+    private Button btnCopy;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         textBeshify = findViewById(R.id.textBeshify);
         btnBeshify = findViewById(R.id.btnBeshify);
         beshifiedText = findViewById(R.id.beshifiedText);
+        btnCopy = findViewById(R.id.btnCopy);
 
         btnBeshify.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
                 String strTextBeshify = textBeshify.getText().toString();
                 String newTextBeshify = strTextBeshify.replace(" ","\uD83E\uDD38");
                 beshifiedText.setText(newTextBeshify);
+            }
+        });
+
+        btnCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clipData = ClipData.newPlainText("",beshifiedText.getText().toString());
+                clipboardManager.setPrimaryClip(clipData);
             }
         });
     }
